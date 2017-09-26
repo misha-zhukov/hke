a5be1936774ac890930199bfd8e8fa6b7ad6c2fb
Misha Zhukov <3v1l91l@gmail.com>
2017-09-26 16:00:21 +0200

    refactor preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,12 @@
 import os
 from PIL import Image
 import numpy as np
+import math
 
 grey_background_color_value = 128
 image_reshape_size = 299
+train_img_folder = "train_img"
+train_ratio = 0.9
 
 def read_img(img_path):
     img = Image.open(img_path)
@@ -14,12 +17,21 @@ def read_img(img_path):
     crop = img.crop(box)
     return crop.resize((image_reshape_size, image_reshape_size), Image.ANTIALIAS)
 
-def copy_imgs_to_label_dirs(data_dir, label_file):
+def preprocess(data_dir, label_file):
     f = open(label_file)
-    images2labels =  dict( l.split(',') for l in f.readlines()[1:])
+    filenames2labels = dict(l.split(',') for l in f.readlines()[1:])
 
+    filenames_shuffled = np.random.permutation(filenames2labels.keys)
+    filenames_num = len(filenames_shuffled)
+    train_filenames_num = math.floor(filenames_num * train_ratio)
+    train_labels = filenames_shuffled[:train_filenames_num]
+    validation_labels = filenames_shuffled[train_filenames_num:]
+
+
+
+def copy_imgs_to_label_dirs(data_dir, label_file):
     i=0
-    for f, l in images2labels.items():
+    for f, l in filenames2labels.items():
         if i%10 == 0:
             dest_path = os.path.join('./validation', l.strip())
         else:
@@ -31,7 +43,7 @@ def copy_imgs_to_label_dirs(data_dir, label_file):
         img.save(os.path.join(dest_path, f + '.png'))
     return
 
-
-ROOT_PATH = "./"
-train_data_dir = os.path.join(ROOT_PATH, "train_img")
-copy_imgs_to_label_dirs(train_data_dir, os.path.join(ROOT_PATH, "train.csv"))
+if __name__ == "__main__":
+    current_path = os.getcwd()
+    train_data_dir = os.path.join(current_path, "train_img")
+    preprocess(train_data_dir, os.path.join(current_path, "train.csv"))
