94d4e6df6d7b8e48b0fe490c2b5ac649376d8599
Misha Zhukov <3v1l91l@gmail.com>
2017-09-27 09:29:26 +0200

    m

--- script.py
+++ script.py
@@ -35,7 +35,6 @@ def read_img(img_path):
     return np.asarray(crop.resize((image_reshape_size, image_reshape_size), Image.ANTIALIAS))
 
 label_list = next(os.walk('train_img'))[1]
-# label_list = train['label'].tolist()
 label_dict = {k:v for v,k in enumerate(set(label_list))}
 
 base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(image_reshape_size, image_reshape_size, 3))
@@ -46,44 +45,44 @@ add_model.add(Dense(256, activation='relu'))
 add_model.add(Dense(len(label_list), activation='softmax'))
 
 model = Model(inputs=base_model.input, outputs=add_model(base_model.output))
-model.compile(loss='categorical_crossentropy', optimizer=optimizers.SGD(lr=2e-3, momentum=0.9, decay=2e-6, nesterov=True),
+model.compile(loss='categorical_crossentropy', optimizer=optimizers.SGD(lr=3e-3, momentum=0.9, decay=2e-6, nesterov=True),
               metrics=['accuracy'])
 
-# batch_size = 90
-# epochs = 10
-#
-# tb = TensorBoard(log_dir='./log', histogram_freq=0,
-#           write_graph=False, write_images=False)
-# model_checkpoint = ModelCheckpoint('inception_v3.model', monitor='val_acc', save_best_only=True)
-# es = EarlyStopping(monitor='val_loss', min_delta=1e-2, patience=4)
-# reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=4)
-# p = Augmentor.Pipeline((os.path.join(os.getcwd(), 'train_categories')),
-#                        output_directory=(os.path.join(os.getcwd(), 'augmentor_output')), save_format="PNG")
-# # p.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
-# p.skew(probability=0.5)
-# p.rotate90(probability=0.5)
-# p.rotate270(probability=0.5)
-# p.crop_random(probability=0.3, percentage_area=0.7)
-# p.resize(probability=1, width=image_reshape_size, height=image_reshape_size)
-# valid_datagen = ImageDataGenerator(rescale=1./255)
-# validation_generator = valid_datagen.flow_from_directory(
-#         (os.path.join(os.getcwd(), 'validation')),
-#         target_size=(image_reshape_size, image_reshape_size),
-#         batch_size=batch_size)
-#
-# history = model.fit_generator(
-#     generator=p.keras_generator(batch_size=batch_size),
-#     steps_per_epoch=(len(label_list) * 0.9) // batch_size,
-#     # steps_per_epoch=2,
-#     epochs=epochs,
-#     callbacks=[
-#         model_checkpoint,
-#         tb,
-#         es,
-#         reduce_lr],
-#     validation_steps=(len(label_list) * 0.1) // batch_size,
-#     validation_data=validation_generator
-# )
+batch_size = 90
+epochs = 10
+
+tb = TensorBoard(log_dir='./log', histogram_freq=0,
+          write_graph=False, write_images=False)
+model_checkpoint = ModelCheckpoint('inception_v3.model', monitor='val_acc', save_best_only=True)
+es = EarlyStopping(monitor='val_loss', min_delta=1e-2, patience=4)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=4)
+p = Augmentor.Pipeline((os.path.join(os.getcwd(), 'train_categories')),
+                       output_directory=(os.path.join(os.getcwd(), 'augmentor_output')), save_format="PNG")
+# p.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
+p.skew(probability=0.5)
+p.rotate90(probability=0.5)
+p.rotate270(probability=0.5)
+p.crop_random(probability=0.3, percentage_area=0.7)
+p.resize(probability=1, width=image_reshape_size, height=image_reshape_size)
+valid_datagen = ImageDataGenerator(rescale=1./255)
+validation_generator = valid_datagen.flow_from_directory(
+        (os.path.join(os.getcwd(), 'validation')),
+        target_size=(image_reshape_size, image_reshape_size),
+        batch_size=batch_size)
+
+history = model.fit_generator(
+    generator=p.keras_generator(batch_size=batch_size),
+    steps_per_epoch=(len(label_list) * 0.9) // batch_size,
+    # steps_per_epoch=2,
+    epochs=epochs,
+    callbacks=[
+        model_checkpoint,
+        tb,
+        es,
+        reduce_lr],
+    validation_steps=(len(label_list) * 0.1) // batch_size,
+    validation_data=validation_generator
+)
 model.load_weights("inception_v3.model")
 test_img = []
 for img_path in tqdm(test['image_id'][:100].values):
