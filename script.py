deb12dc1be40c583b9e196873cee876a1665dc43
Misha Zhukov <3v1l91l@gmail.com>
2017-09-27 16:32:11 +0200

    m

--- script.py
+++ script.py
@@ -49,40 +49,40 @@ add_model.add(Dense(256, activation='relu'))
 add_model.add(Dense(len(label_list), activation='softmax'))
 
 model = Model(inputs=base_model.input, outputs=add_model(base_model.output))
-model.compile(loss='categorical_crossentropy', optimizer=optimizers.SGD(lr=0.02, momentum=0.9, decay=1e-6, nesterov=True),
+model.compile(loss='categorical_crossentropy', optimizer=optimizers.SGD(lr=0.05, momentum=0.9, decay=1e-6, nesterov=True),
               metrics=['accuracy'])
 print("Compiled model in: {}s".format(time_start-time.time()))
 
-#batch_size = 90
-#epochs = 10
+batch_size = 90
+epochs = 10
 
-#tb = TensorBoard(log_dir='./log', histogram_freq=0,
-#          write_graph=False, write_images=False)
-#model_checkpoint = ModelCheckpoint('inception_v3.model', monitor='val_acc', save_best_only=True, save_weights_only=True)
-#es = EarlyStopping(monitor='val_loss', min_delta=1e-2, patience=4)
-#p = Augmentor.Pipeline((os.path.join(os.getcwd(), 'train_categories')),
-#                       output_directory=(os.path.join(os.getcwd(), 'augmentor_output')), save_format="PNG")
-#p.skew(probability=0.3)
-#p.rotate90(probability=0.3)
-#p.rotate270(probability=0.3)
-#p.crop_random(probability=0.3, percentage_area=0.8)
-#p.resize(probability=1, width=image_reshape_size, height=image_reshape_size)
-#valid_datagen = ImageDataGenerator(rescale=1./255)
-#validation_generator = valid_datagen.flow_from_directory(
-#        (os.path.join(os.getcwd(), 'validation_categories')),
-#        target_size=(image_reshape_size, image_reshape_size),
-#        batch_size=batch_size)
+tb = TensorBoard(log_dir='./log', histogram_freq=0,
+         write_graph=False, write_images=False)
+model_checkpoint = ModelCheckpoint('inception_v3.model', monitor='val_acc', save_best_only=True, save_weights_only=True)
+es = EarlyStopping(monitor='val_loss', min_delta=1e-2, patience=4)
+p = Augmentor.Pipeline((os.path.join(os.getcwd(), 'train_categories')),
+                      output_directory=(os.path.join(os.getcwd(), 'augmentor_output')), save_format="PNG")
+p.skew(probability=0.2)
+p.rotate90(probability=0.3)
+p.rotate270(probability=0.3)
+p.crop_random(probability=0.3, percentage_area=0.9)
+p.resize(probability=1, width=image_reshape_size, height=image_reshape_size)
+valid_datagen = ImageDataGenerator(rescale=1./255)
+validation_generator = valid_datagen.flow_from_directory(
+       (os.path.join(os.getcwd(), 'validation_categories')),
+       target_size=(image_reshape_size, image_reshape_size),
+       batch_size=batch_size)
 
-#history = model.fit_generator(
-#    generator=p.keras_generator(batch_size=batch_size),
-#    steps_per_epoch=(train_image_num * 0.9) // batch_size,
-#    epochs=epochs,
-#    callbacks=[
-#        model_checkpoint,
-#        tb,
-#        es],
-#    validation_steps=(train_image_num * 0.1) // batch_size,
-#    validation_data=validation_generator)
+history = model.fit_generator(
+   generator=p.keras_generator(batch_size=batch_size),
+   steps_per_epoch=(train_image_num * 0.9) // batch_size,
+   epochs=epochs,
+   callbacks=[
+       model_checkpoint,
+       tb,
+       es],
+   validation_steps=(train_image_num * 0.1) // batch_size,
+   validation_data=validation_generator)
 model.load_weights("inception_v3.model")
 test_img = []
 for img_path in tqdm(test['image_id'].values):
@@ -93,21 +93,21 @@ predictions = np.argmax(predictions, axis=1)
 rev_y = {v:k for k,v in label_dict.items()}
 pred_labels = [rev_y[k] for k in predictions]
 
-sub = pd.DataFrame({'image_id': test.image_id, 'label': pred_labels})
+sub = pd.DataFrame({'image_id': test['image_id'], 'label': pred_labels})
 sub.to_csv('sub.csv', index=False)
 
-# plt.plot(history.history['acc'])
-# plt.plot(history.history['val_acc'])
-# plt.title('model accuracy')
-# plt.ylabel('accuracy')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.savefig('acc')
-#
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('model loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.savefig('loss')
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('acc')
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('loss')
